5a9ffb31196743e32a78f325acec319224485062
Sébastien Conejo <sebastien.conejo@buddyweb.fr>
2019-03-17 11:20:36 +0100

    Update environnement

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:5555/'
+  // apiBaseUrl: 'http://localhost:5555/'
+  apiBaseUrl: 'https://datathon-api.atelier.buddyweb.fr/'
 }
 
 /*
